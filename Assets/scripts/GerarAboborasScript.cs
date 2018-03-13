b70700baf23f48c24f0bedb589c13f25f5faa22d
logonrm <logonrm@fiap.com.br>
2018-03-12 22:46:47 -0300

    zumbi

---
+++ Assets/scripts/GerarAboborasScript.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GerarAboborasScript : MonoBehaviour {
+
+	public float limiteEsquerdo, limiteDiretiro, limteFronal, limiteTraseiro;
+	public float frequencia;
+	public GameObject aboboraPrefab;
+
+	// Use this for initialization
+	IEnumerator Start () {
+		yield return new WaitForSeconds (Random.Range(0.5f, frequencia));
+		Vector3 posicao;
+		posicao.x = Random.Range (limiteEsquerdo, limiteDiretiro);
+		posicao.y = transform.position.y;
+		posicao.z = Random.Range (limteFronal, limiteTraseiro);
+		Instantiate (aboboraPrefab, posicao, transform.rotation);
+		StartCoroutine (Start());
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ Assets/scripts/PersonagemScript.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PersonagemScript : MonoBehaviour {
+	Vector3 novaPosicao;
+	public float velocidade;
+	public GameObject personagem;
+	Animation ani;
+
+
+
+	// Use this for initialization
+	void Start () {
+		//Capura a posicao inicial do prsonagem
+		novaPosicao = transform.position;
+		ani = personagem.GetComponent<Animation> ();
+
+		//deine a animacao inicial
+		ani.CrossFade("idle");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		//touch ou clique do mouse
+		if (Input.GetButton ("Fire1")) {
+			RaycastHit hit;
+			Ray ray = Camera.main.ScreenPointToRay (Input.mousePosition);
+
+			//obtem a nova posicao
+			if (Physics.Raycast (ray, out hit)) {
+				novaPosicao = hit.point;
+
+			}
+			//move o personagem
+			transform.position = Vector3.MoveTowards (transform.position, novaPosicao, velocidade * Time.deltaTime);
+			ani.CrossFade("run");
+			transform.LookAt (hit.point);
+
+		} else {
+			//animacao de ile
+			ani.CrossFade("idle");
+		}
+
+
+
+	}
+
+	void OnCollisionEnter(Collision c){
+		if(c.gameObject.tag == "item"){
+			Destroy (c.gameObject);
+		}
+	}
+}
